479c9bbf08003e8aeac9be9908bb7ebf8143811b
Dan Berkeland <danberkeland@gmail.com>
2022-11-04 08:09:14 -0700

    change pickup time button on cart

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,16 +3,27 @@ import React, { useRef } from "react";
 import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
 import { InputNumber } from "primereact/inputnumber";
-
+import { time_convert, date_convert, checkAvailable, itemFilter } from "./utils";
 import { Toast } from "primereact/toast";
 import { useSettingsStore } from "../Contexts/SettingsZustand";
+import { Title, SubInfo } from "../CommonStyles";
 
 function Cart({ displayCart, setDisplayCart }) {
+  const delivDate = useSettingsStore((state) => state.delivDate);
+  const delivTime = useSettingsStore((state) => state.delivTime);
+  const location = useSettingsStore((state) => state.location);
   const cartOrder = useSettingsStore((state) => state.cartOrder);
+  const setFormType = useSettingsStore((state) => state.setFormType);
   const toast = useRef(null);
 
   const onHide = () => {
     setDisplayCart(false);
+  };
+
+  
+  const handleChangePickup = () => {
+    setDisplayCart(false);
+    setFormType("");
   };
 
   return (
@@ -23,33 +34,80 @@ function Cart({ displayCart, setDisplayCart }) {
         style={{ width: "50vw", minWidth: "300px" }}
         onHide={onHide}
       >
+        <Title>Cart</Title>
+        <div className="tabInfo lgscreen">
+          <div>For pickup at&nbsp;&nbsp; </div>
+          <h2>{location}&nbsp;&nbsp;</h2>
+          <div> on &nbsp;&nbsp; </div>
+          <h2>{date_convert(delivDate)}&nbsp;&nbsp;</h2>
+          <div> at&nbsp;&nbsp; </div>
+          <h2> {time_convert(delivTime * 60)}</h2>
+        </div>
+        <div className="smscreen medscreen">
+          <Button
+            type="button"
+            label={
+              location +
+              ` - ` +
+              date_convert(delivDate) +
+              " - " +
+              time_convert(delivTime * 60)
+            }
+            className="p-button-text p-button-warning"
+            aria-label="Bookmark"
+            onClick={handleChangePickup}
+          />
+        </div>
+        <div className="lgscreen">
+          <Button
+            type="button"
+            icon="pi pi-clock"
+            label="Change Pickup"
+            className="p-button-raised"
+            aria-label="Bookmark"
+            onClick={handleChangePickup}
+          />
+        </div>
         {cartOrder.map((cart) => {
           return (
-            <React.Fragment>
-              <Button label="REMOVE"/>
-              <div>{cart.qty}</div>
-              <InputNumber
-                inputId="horizontal"
-                disabled={false}
-                value={1}
-               
-                size={5}
-                showButtons
-                buttonLayout="horizontal"
-                min={0}
-                decrementButtonClassName="p-button-secondary"
-                incrementButtonClassName="p-button-secondary"
-                incrementButtonIcon="pi pi-plus"
-                decrementButtonIcon="pi pi-minus"
+            <div className="cartItem">
+               <Button icon="pi pi-times" className="p-button-outlined p-button-rounded p-button-danger" aria-label="Cancel" />
+              <img
+                className="foodPicSmall"
+                src={cart.item.url}
+                alt="sandwich"
               />
-              <div>{cart.item.name}</div>
-              {cart.modifiers.map(mod => {
-                return <div>{mod.name}</div>
-              })}
-              <div>{cart.price}</div>
-            </React.Fragment>
+              <div>
+                <div>{cart.item.name}</div>
+                {cart.modifiers.map((mod) => {
+                  return <div>{mod.name}</div>;
+                })}
+              </div>
+
+              <div>
+                <InputNumber
+                  inputId="horizontal"
+                  disabled={false}
+                  value={1}
+                  size={2}
+                  showButtons
+                  buttonLayout="horizontal"
+                  min={0}
+                  decrementButtonClassName="p-button-secondary"
+                  incrementButtonClassName="p-button-secondary"
+                  incrementButtonIcon="pi pi-plus"
+                  decrementButtonIcon="pi pi-minus"
+                />
+                <div>${cart.price.toFixed(2)}</div>
+              </div>
+
+              
+              
+            </div>
           );
         })}
+        <div>TOTAL</div>
+        <Button label="PAY NOW" />
       </Dialog>
     </React.Fragment>
   );
