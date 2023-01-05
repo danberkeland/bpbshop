9e306b7e2d4a8660cd04139dd5b966be9cf075e1
Dan Berkeland <danberkeland@gmail.com>
2023-01-05 11:27:38 -0800

    fixed date adding extra 0

--- src/restAPIs.js
+++ src/restAPIs.js
@@ -63,7 +63,7 @@ export const infoChosen = (event, location, init, setInit) => {
     newDate.getFullYear() +
     "-" +
     (newDate.getMonth() + 1 < 10
-      ? "0" + newDate.getMonth() + 1
+      ? "0" + (newDate.getMonth() + 1)
       : newDate.getMonth() + 1) +
     "-" +
     (newDate.getDate() < 10 ? "0" + newDate.getDate() : newDate.getDate());
